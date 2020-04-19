c12e20ef6b6f55bf84718313235d8b2138befcc1
Yegor Ganin <63185173+yeegorski@users.noreply.github.com>
2020-04-19 11:23:30 +0200

    Create data_structure_documentation.sh

---
+++ data_structure_documentation.sh
@@ -0,0 +1,33 @@
+# Event types
+
+	registration
+	sent_a_free_tree
+	sent_a_super_tree
+
+# Attributes for the events
+
+	## registration
+
+		file_dame
+		date
+		time
+		user_id
+		event [registration]
+		birth year [years between 1970 and 2000]
+		device_type [ios, android, error]
+		country [germany, brazil, united states, sweden, philippines]
+		source [google, invite_a_friend, article, paid]
+
+	## sent_a_free_tree
+
+		file_name
+		date
+		user_id
+		event [sent_a_free_tree]
+
+	## sent_a_super_tree
+
+		file_name
+		date
+		user_id
+		event [sent_a_super_tree]
