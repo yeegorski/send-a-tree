353b581733f38f72e24a9a9cc2389612c57b8af0
Yegor Ganin <63185173+yeegorski@users.noreply.github.com>
2020-04-19 11:15:01 +0200

    Create data_download.sh

---
+++ Week 1/data_download.sh
@@ -0,0 +1,18 @@
+#downloading the data log files from a server
+#on the server every file is named day_x, day_1 being September 13, 2019
+#as of the day the code was written (April 19, 2020), there were 219 data log files (219 days) on the server 
+
+#creating a folder for the data log files
+mkdir raw_data
+
+#entering the folder
+cd raw_data
+
+#in the folder raw_data, the actual download
+start=1
+end=220
+while [ $start -lt $end ]
+do
+wget 0.0.0.0/folder/day_$i
+i=$((i+1))
+done
